3d228d9108420924bde52f2511302983b70714b9
Nitin Yadav <nitinyadav3720@gmail.com>
2022-11-26 10:49:20 +0530

    add TreeNode constructor tests

--- tests/tree.test.js
+++ tests/tree.test.js
@@ -1,8 +1,43 @@
 import test from "ava";
 import TreeNode from "../utils/tree.js";
 
-test("TreeNode constructor zero argument", (t) => {
-    let node = new TreeNode();
-    t.is(node.val, 0);
-    t.is(node.char, "");
+test("Invalid constructor arguments", t => {
+    //no arguments
+    try{
+        let node = new TreeNode();
+    }
+    catch( e ){
+        t.is( e.name, "InvalidConstructorArguments");
+    }
+    
+    //invalid args
+    let args = [
+        [null, null], 
+        [undefined, undefined], 
+        [true, true], 
+        [false, false],
+        [true, false],
+        [false, true],
+        ["",""],
+        [1, 1],
+        ["", 1]
+    ];
+    args.forEach( arg => {
+        try{
+            let node = new TreeNode(arg[0], arg[1]);
+        }
+        catch(e){
+            t.is(e.name, "InvalidConstructorArguments");
+        }
+    });
+});
+
+test("Valid constructor arguments", t => {
+    let node1 = new TreeNode(1);
+    t.is(node1.val, 1);
+    t.is(node1.char, "");
+
+    let node2 = new TreeNode(2, "a");
+    t.is(node2.val, 2);
+    t.is(node2.char, "a");
 });
