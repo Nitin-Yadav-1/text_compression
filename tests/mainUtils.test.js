763a4e2269c2a1bc7a6e549d845a22c5240320ac
Nitin Yadav <nitinyadav3720@gmail.com>
2022-11-27 14:21:30 +0530

    add test for buildHuffmanTree fn in mainUtils.js

--- tests/mainUtils.test.js
+++ tests/mainUtils.test.js
@@ -1,9 +1,23 @@
 import test from 'ava';
-import {getCountOfCharacters} from "../utils/mainUtils.js";
+import {getCountOfCharacters, buildHuffmanTree} from "../utils/mainUtils.js";
 
 test("test function getCountOfCharacters()", t => {
     t.deepEqual( getCountOfCharacters(), [] );
     t.deepEqual( getCountOfCharacters(3), [] );
     t.deepEqual( getCountOfCharacters(2,"abc"), [] );
     t.deepEqual( getCountOfCharacters("a"), [["a",1]] );
+});
+
+test("test function buildHuffmanTree", t => {
+    let root1 = buildHuffmanTree();
+    t.is(root1, undefined);
+
+    let root2 = buildHuffmanTree( getCountOfCharacters("") );
+    t.is(root2, undefined);
+
+    let root3 = buildHuffmanTree( [] );
+    t.is( root3, undefined);
+
+    let root4 = buildHuffmanTree( getCountOfCharacters("abbcccdddd"));
+    t.deepEqual(root4.getValuesInPreorder(), [10, 4, 6, 3, 3, 1, 2]);
 });
